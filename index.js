5d8eb9879f68d503a654079408db7425e7489abd
saivishnu725 <saivishnu725@gmail.com>
2024-06-20 21:10:34 +0530

    foundation of the site

---
+++ index.js
@@ -0,0 +1,49 @@
+// import packages
+import express from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import mongoose from 'mongoose';
+
+// import express routes
+import authRoutes from "./routes/auth.js";
+
+// .env config
+dotenv.config();
+
+// Connect to MongoDB
+console.log('Connecting to MongoDB');
+await mongoose.connect('mongodb://127.0.0.1:27017/quizify').then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.error('Failed to connect to MongoDB', err);
+});
+
+// Express
+const app = express();
+
+//body-parser
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//path
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// API routes
+app.get('/', (req, res) => {
+    res.json({
+        message: "Welcome to Quizify!"
+    });
+});
+
+// AUTH routes
+app.use("/api/auth", authRoutes);
+
+// Server
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, (err) => {
+    if (err) throw err;
+    console.log(`Server running on http://localhost:${PORT}`);
+});
