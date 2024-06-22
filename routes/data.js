1bacceabc6cb3caa64059b906ade470ce82fc873
saivishnu725 <saivishnu725@gmail.com>
2024-06-22 17:21:25 +0530

    template with todo

---
+++ routes/data.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+const router = Router();
+
+// TODO: get: user info route
+router.post("/user-info", method);
+
+// TODO: get user full profile
+// TODO: get: user quiz details
+// TODO: get: quiz data
+// TODO: get: quiz results
+// TODO: post: update user details
+
+
+export default router;
