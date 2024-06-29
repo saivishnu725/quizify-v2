b91b7f642926da26382ab18f3bd1d2bf5c588d34
saivishnu725 <saivishnu725@gmail.com>
2024-06-29 20:54:27 +0530

    ADD POST /api/create-quiz template

--- routes/data.js
+++ routes/data.js
@@ -2,7 +2,15 @@ import { Router } from "express";
 const router = Router();
 
 // TODO: get: user info route
-router.post("/user-info", method);
+// router.post("/user-info", method);
+
+// TODO: post: create quiz
+router.post("/create-quiz", (req, res) => {
+    console.log("Form posted");
+    // TODO: create quiz using req.body
+    console.log("Quiz created: ", req.body);
+    res.status(201).json({ message: "Quiz created successfully", quiz: req.body });
+});
 
 // TODO: get user full profile
 // TODO: get: user quiz details
