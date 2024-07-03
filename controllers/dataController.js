120aa3ece41401b3094535901e98aa97e94e4c69
saivishnu725 <saivishnu725@gmail.com>
2024-07-03 16:43:52 +0530

    save new properties

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -1,14 +1,16 @@
 import User from "../models/user.js"; // Import the User model
-import QuizCollection from "../models/quiz_collection.js"; // Import the User model
+import QuizCollection from "../models/quiz_collection.js"; // Import the QuizCollection model | stores the entire quiz
+import Quiz from "../models/quiz.js"; // Import the Quiz model | stores the basic quiz details
 
+// Get user details
 export async function getUserDetails(id) {
     try {
         const user = await User.findOne({ where: { id: id } });
         if (!user) {
             return 'User not found';
         }
-        const { last_name, email } = user;
-        return { id, last_name, email };
+        const { username, first_name, last_name, email } = user;
+        return { id, username, first_name, last_name, email };
     } catch (error) {
         console.error(error);
         return 'An error occurred where requesting user ' + id + '\'s details';
@@ -19,10 +21,44 @@ export async function getUserDetails(id) {
 export async function saveQuiz(quizData, userId) {
     try {
         console.log("quizData:" + JSON.stringify(quizData));
+
+        // quiz tag generation
+        const quizTag = userId + quizData.title.replace(/\s+/g, '-').toLowerCase();
+        console.log("Quiz Tag: " + quizTag);
+
+        // 1. Save the quiz in MariaDB
+        let newQuiz = {};
+        try {
+            newQuiz = await Quiz.create({
+                title: quizData.title,
+                description: quizData.description,
+                quizTag: quizTag,
+                creator_id: userId,
+                maxTime: quizData.maxTime,
+                maxPlayers: quizData.maxPlayers,
+                score: quizData.score,
+                negativeScore: quizData.negativeScore,
+                joinTime: quizData.joinTime,
+            });
+        } catch (e) {
+            console.error("Error saving quiz in MariaDB:", e);
+        }
+
+        // Retrieve the quiz_id
+        const quizId = newQuiz.id;
+        console.log("Quiz ID in MariaDB: " + quizId);
+        // 2. Save the quiz in MongoDB
+
         const quiz = new QuizCollection({
-            quiz_id: quizData.quiz_id, // TODO: Handle unique ID generation
+            quiz_id: quizId,
+            quiz_tag: quizTag,
             title: quizData.title,
             description: quizData.description,
+            maxTime: quizData.maxTime,
+            maxPlayers: quizData.maxPlayers,
+            score: quizData.score,
+            negativeScore: quizData.negativeScore,
+            joinTime: quizData.joinTime,
             questions: (quizData.quiz || []).map((q, index) => ({
                 question_id: index + 1,
                 question_type: q.type,
@@ -38,7 +74,10 @@ export async function saveQuiz(quizData, userId) {
         console.log('Quiz data:', JSON.stringify(quiz));
         await quiz.save();
         console.log('Quiz saved in mongo successfully');
-        return quiz.quiz_id;
+        // 3. Return the quiz tag and ID
+        console.log("Quiz ID right before submitting: ", quizId);
+        return { quizTag, quizId };
+        // return { quiz.quizTag, quiz.quiz_id };
     } catch (error) {
         console.error('Error saving quiz in mongo:', error);
     }
