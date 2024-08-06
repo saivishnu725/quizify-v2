ca1453c5081b089126fad14ea0a25949b5374801
saivishnu725 <saivishnu725@gmail.com>
2024-08-06 13:38:23 +0530

    table name update

--- models/userQuiz.js
+++ models/userQuiz.js
@@ -5,9 +5,12 @@ dotenv.config();
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'mariadb',
+    define: {
+        timestamps: false,
+    },
 });
 
-const UserQuiz = sequelize.define('UserQuiz', {
+const UserQuiz = sequelize.define('user_quizzes', {
     id: { type: DataTypes.BIGINT.UNSIGNED, primaryKey: true, autoIncrement: true },
     user_id: { type: DataTypes.BIGINT, allowNull: false },
     quiz_at: { type: DataTypes.BIGINT, allowNull: false },
