0efacf494500605966e43e2a09aa5457c328867c
saivishnu725 <saivishnu725@gmail.com>
2024-08-06 13:40:21 +0530

    handle POST /submit-quiz

--- routes/data.js
+++ routes/data.js
@@ -2,6 +2,9 @@ import jwt from 'jsonwebtoken';
 import { Router } from "express";
 import dotenv from 'dotenv';
 import { saveQuiz } from '../controllers/dataController.js';
+import UserQuiz from '../models/userQuiz.js';
+import UserQuizAnswersCollection from '../models/userQuizAnswersCollection.js';
+import QuizCollection from '../models/quizCollection.js';
 
 const router = Router();
 dotenv.config();
@@ -13,7 +16,6 @@ const secret = process.env.JWT_SECRET || "secret";
 // TODO: post: create quiz
 router.post("/create-quiz", async (req, res) => {
     console.log("Form posted");
-    // TODO: create quiz using req.body
     console.log("Quiz created: ", req.body);
     console.log("Token: ", req.session.token);
     let userInfo = {};
@@ -30,6 +32,104 @@ router.post("/create-quiz", async (req, res) => {
     console.log("Quiz Tag: ", quizTag);
     res.status(201).json({ message: "Quiz created successfully", quiz: req.body, userID: userInfo.id, quizId: quizId, quizTag: quizTag });
 });
+
+// Handle quiz submission
+router.post('/submit-quiz', async (req, res) => {
+    console.log('Quiz submitted');
+    console.log('Quiz body in submit-quiz: ', req.body);
+
+    try {
+        // 1. Get the current userID from session token.
+        let userInfo = {};
+        jwt.verify(req.session.token, secret, (err, user) => {
+            if (err) {
+                console.error("Error in jwtTokenVerify:", err.message);
+                return res.status(401).json({ message: "Unauthorized" });
+            }
+            userInfo = user || {};
+        });
+        const userId = userInfo.id;
+        console.log('userID: ', userId);
+        console.log('Quiz answers: ', req.body['answers']);
+        const answers = JSON.parse(req.body.answers); // array of selected options per question
+        console.log('answers: ', answers);
+        const quizTag = req.body.quizTag;
+        console.log('quizTag: ', quizTag);
+        let correctAnswersCount = 0;
+
+        // 2. Retrieve the quiz data
+        const quiz = await QuizCollection.findOne({ quiz_tag: quizTag });
+        if (!quiz) return res.status(404).json({ message: "Quiz not found" });
+
+        const totalQuestions = quiz.questions.length;
+
+        // 3. Calculate the correct answers
+        let userAnswers = [];
+        try {
+            userAnswers = quiz.questions.map((q, index) => {
+                // console.log('q:', q);
+                console.log('answers[index]:', answers[index]);
+                const correct = q.correctOption === parseInt(answers[index]);
+                if (correct) correctAnswersCount++;
+                return {
+                    question_id: q.question_id,
+                    selected_option_id: parseInt(answers[index])
+                };
+            });
+            console.log('correct answers count:', correctAnswersCount);
+        } catch (error) {
+            console.log("Error in calculating correct answers:", error);
+            res.status(500).json({ message: "Internal server error", error: error });
+            return;  // won't continue
+        }
+
+        const wrongAnswersCount = totalQuestions - correctAnswersCount;
+        const finalPercentage = (correctAnswersCount / totalQuestions) * 100;
+
+        // 4. Save data in UserQuiz
+        try {
+            const userQuiz = await UserQuiz.create({
+                user_id: userId,
+                quiz_at: quiz.quiz_id
+            });
+        } catch (error) {
+            console.error("Error in saving userQuiz in UserQuiz collection:", error);
+            res.status(500).json({ message: "Internal server error", error: error });
+            return;  // won't continue
+        }
+
+        // 5. Save data in UserQuizAnswersCollection
+        try {
+            const userQuizAnswers = new UserQuizAnswersCollection({
+                user_id: userId,
+                quiz_id: quiz.quiz_id,
+                answers: userAnswers
+            });
+            console.log('UserQuizAnswers:', userQuizAnswers);
+            await userQuizAnswers.save();
+        } catch (error) {
+            console.error("Error in saving userQuizAnswers in UserQuizAnswersCollection:", error);
+            res.status(500).json({ message: "Internal server error", error: error });
+            return;  // won't continue
+        }
+        // 6. Wrap the final result into JSON and redirect to quiz-result
+        const result = {
+            questionsCount: totalQuestions,
+            correctQuestions: correctAnswersCount,
+            wrongQuestions: wrongAnswersCount,
+            finalPercentage: finalPercentage,
+            quizTitle: quiz.title
+        };
+
+        res.status(200).json(result);
+
+    } catch (error) {
+        console.error("Error in submitting quiz:", error);
+        res.status(500).json({ message: "Internal server error", error: error });
+    }
+
+});
+
 
 // TODO: get user full profile
 // TODO: get: user quiz details
